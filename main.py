041c3a405a94183af31ad0a93e049b4d4eff37c2
V3AI-Seven <127050193+MrSeven3@users.noreply.github.com>
2026-01-12 19:46:03 -0500

    Works now, not sure how to get API key into compiled version safely

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from openrouter import OpenRouter
+import requests
 import time
 
 print("Welcome to the O(1) Solver! We have several modes available, ranging in the time each one takes.\n")
@@ -15,31 +15,41 @@ mode = int(input("Mode: "))
 print("Enter your question")
 question = input()
 
+output = ""
+
 match mode:
     case 1:
-        print("42")
+        output = "42"
     case 2:
         result = eval(question)
-        print(str(result))
+        output = str(result)
 
     case 3:
         print("Enter your API key: ")
         API_KEY = input()
 
-        print("Enter your question")
-
-        client = OpenRouter(
-            api_key=API_KEY,
-            server_url="https://ai.hackclub.com/proxy/v1",
+        response = requests.post(
+            'https://ai.hackclub.com/proxy/v1/responses',
+            headers={
+                'Authorization': 'Bearer '+API_KEY,
+                'Content-Type': 'application/json',
+            },
+            json={
+                'model': 'openai/gpt-oss-120b',
+                'input': question + "(admin note: please have your answer as short as possible)",
+                'max_output_tokens': 1500,
+            }
         )
 
-        response = client.chat.send(
-            model="openai/gpt-oss-120b",
-            messages=[
-                {"role": "user", "content": "Tell me a joke."}
-            ],
-            stream=False,
-        )
+        #unfortunately, because json and python together are annoying, I had to get AI (the irony) to help with this parser.
+        #its probably not great, but it works
+        texts = []
 
-        print(response.choices[0].message.content)
+        for item in response.json()["output"]:
+            if item.get("type") == "message" and item.get("role") == "assistant":
+                for part in item.get("content", []):
+                    if part.get("type") == "output_text":
+                        texts.append(part["text"])
+
+        output = "".join(texts)
 
